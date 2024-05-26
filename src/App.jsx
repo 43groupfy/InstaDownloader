ad532a1434fff4216a4667b0ed08bbcdca02cc20
Hamza Younis <hamzayounis106@gmail.com>
2024-05-26 05:33:25 +0500

    error fix

--- src/App.jsx
+++ src/App.jsx
@@ -48,21 +48,43 @@ function App() {
     <div className="bg-dark text-gray-100 bg-zinc-900 flex justify-center items-start h-screen">
       <div className="container mx-auto py-8 px-4">
         <form onSubmit={handleSubmit}>
-        <label for="videoUrl" class="block mb-2">Enter Insta Reel URL here</label>
-          <input type="text" onChange={handleChange} name="videoUrl" value={inputValue} className="w-full px-4 py-2 rounded-md bg-gray-800 text-gray-100 focus:outline-none focus:ring-2 focus:ring-clifford" />
-          <button type="submit" className="mt-4 px-6 py-2 bg-zinc-100 rounded-md text-zinc-900 font-bold">Submit</button>
+          <label htmlFor="videoUrl" className="block mb-2">
+            Enter Insta Reel URL here
+          </label>
+          <input
+            type="text"
+            onChange={handleChange}
+            name="videoUrl"
+            value={inputValue}
+            className="w-full px-4 py-2 rounded-md bg-gray-800 text-gray-100 focus:outline-none focus:ring-2 focus:ring-clifford"
+          />
+          <button
+            type="submit"
+            className="mt-4 px-6 py-2 bg-zinc-100 rounded-md text-zinc-900 font-bold"
+          >
+            Submit
+          </button>
         </form>
         <div>
-        {resData === null ? null : resData === 404 ? (
-          <div>Invalid URL</div>
-        ) : (
-          <div className="flex  items-center flex-col gap-5">
-            <img src={resData.thumb} alt="Image" className="h-[40vh] rounded-md" />
-            <a target="_blank" rel="noopener noreferrer" href={resData.link} className="inline-block  rounded-md mt-4 px-6 py-2 bg-[#16a34a] text-zinc-100 font-bold">
-              Download
-            </a>
-          </div>
-        )}
+          {resData === null ? null : resData === 404 ? (
+            <div>Invalid URL</div>
+          ) : (
+            <div className="flex  items-center flex-col gap-5">
+              <img
+                src={resData.thumb}
+                alt="Image"
+                className="h-[40vh] rounded-md"
+              />
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href={resData.link}
+                className="inline-block  rounded-md mt-4 px-6 py-2 bg-[#16a34a] text-zinc-100 font-bold"
+              >
+                Download
+              </a>
+            </div>
+          )}
         </div>
       </div>
     </div>
